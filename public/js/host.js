8242b18b2e077affbc4833d1b266ae54f00467c4
Alex Osterloh <aosterloh@google.com>
2026-08-11 14:12:09 +0200

    Embed Chrome Lonely T-Rex dinosaur icon in the center of the generated QR code

--- public/js/host.js
+++ public/js/host.js
@@ -145,20 +145,15 @@ async function loadQrCode(targetCode) {
   }
   if (qrCodeImg) {
     if (typeof window.generateClientQrDataUri === 'function') {
-      const clientQrDataUri = window.generateClientQrDataUri(playerUrl);
+      const clientQrDataUri = window.generateClientQrDataUri(playerUrl, (asyncDataUri) => {
+        if (asyncDataUri && qrCodeImg) {
+          qrCodeImg.src = asyncDataUri;
+        }
+      });
       if (clientQrDataUri) {
         qrCodeImg.src = clientQrDataUri;
       }
     }
-  }
-  try {
-    const res = await fetch(`/api/qr?room=${encodeURIComponent(targetCode || roomCode)}`);
-    const data = await res.json();
-    if (data.qrUri && qrCodeImg) {
-      qrCodeImg.src = data.qrUri;
-    }
-  } catch (err) {
-    console.error("Failed to load server QR code:", err);
   }
 }
 loadQrCode(roomCode);
